4ddd960f87f0182f1cf6c16df954ced4325d0962
Jeff <Jeff@Cindy-PC>
2018-09-14 10:26:56 -0500

    initial commit

---
+++ groupproject/src/main/java/edu/umsl/cs5012/groupproject/GroupprojectApplication.java
@@ -0,0 +1,12 @@
+package edu.umsl.cs5012.groupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupprojectApplication.class, args);
+	}
+}
---
+++ groupproject/src/main/java/edu/umsl/cs5012/groupproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.umsl.cs5012.groupproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GroupprojectApplication.class);
+	}
+
+}
