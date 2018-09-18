d5565a6766e3ff5390a242ab62711c51b5a60010
Jeff <Jeff@Cindy-PC>
2018-09-18 16:26:49 -0500

    added a test method for demo

--- groupproject/src/test/java/edu/umsl/cs5012/groupproject/GroupprojectApplicationTests.java
+++ groupproject/src/test/java/edu/umsl/cs5012/groupproject/GroupprojectApplicationTests.java
@@ -1,9 +1,11 @@
 package edu.umsl.cs5012.groupproject;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,5 +14,16 @@ public class GroupprojectApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+	
+	
+	@Test
+	public void testMyRepository() {
+		String name="Jeff";
+		Assert.assertEquals("Jeff", name);
+		
+		Assert.assertNull(name);
+		
+	}
+	
 
 }
